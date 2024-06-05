6e7edb7a911739ec20be9c4d7af4148c6e9c5aad
igtm <github@igtm.link>
2024-06-05 17:05:06 +0900

    lazyVim

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- escape with jj
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
+-- save file
+-- vim.keymap.set("n", "ww", ":w<CR>", { silent = true })
